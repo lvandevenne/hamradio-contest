66dceafdf8f134fca77557d2650ef3842aa4b636
Lies <lies@donschufro.home.bitnet.be>
2017-03-14 14:44:33 +0100

    created basic mvn domain project

---
+++ hamradio-domain/src/test/java/be/bitnet/hamradio/AppTest.java
@@ -0,0 +1,15 @@
+package be.bitnet.hamradio;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class QsoTest 
+    extends TestCase
+{
+
+    public static constructor_works (){
+			Qso qso = new Qso();
+			assertTrue(qso != null);
+    }
+}
